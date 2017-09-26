06b2d133e6143164a6ac4816ded48e85ec0f4e39
ram194 <ram194@login0a.frank.sam.pitt.edu>
2017-09-26 14:53:58 -0400

    works with the sample phenotype file

--- DESeq.sh
+++ DESeq.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env Rscript 
 
 source("https://bioconductor.org/biocLite.R")
-biocLite(c("DESeq2","RcolorBrewer","ggplot2","gplots","Annotate")) 
+biocLite("DESeq2") 
 library(DESeq2)
 library(annotate)
+library(org.Mm.eg.db)
+library(pheatmap)
 library(RColorBrewer)
 library(ggplot2)
 library(gplots)
 
 ## Download necessary modules 
+samples=read.table("./sample_phenotype.txt",sep="\t",header=FALSE)
+countdata <- read.table("./htseq_count.txt",sep="\t",header=TRUE)
+rownames(countdata)=countdata[,1]
+counts=countdata[,-1]
 
-countdata <- readRDS("counts_0530.rds")
-counts=countdata$counts
-colnames(counts) <- gsub("\\.[sb]am$", "", colnames(counts))
-condition <- factor(c(rep("C", 12), rep("T", 12)))
+#counts=countdata$counts
+#colnames(counts) <- gsub("\\.[sb]am$", "", colnames(counts))
+condition <- factor(c(rep("C", length(samples[samples$V2 == "Control",])), rep("T", length(samples[samples$V2 =="Case",]))))
 coldata <- data.frame(row.names=colnames(counts), condition)
 dds <- DESeqDataSetFromMatrix(countData=counts, colData=coldata, design=~condition)
 rld <- rlogTransformation(dds)
@@ -24,13 +29,12 @@ resdata <- merge(as.data.frame(res), as.data.frame(counts(dds, normalized=TRUE))
 write.table(resdata,"DGEtable.txt",sep="\t",row.names = T,col.names = T)
 
 ##plot PCA
-plotPCA(rld, intgroup="condition")
+
+#plotPCA(rld, intgroup="condition")
 data <- plotPCA(rld, intgroup="condition", returnData=TRUE) 
 percentVar <- round(100 * attr(data, "percentVar"))
+png(filename ="PCA.png",width = 700, height=800)
 ggplot(data, aes(PC1, PC2, color=condition)) + geom_point(size=3) + xlab(paste0("PC1: ",percentVar[1],"% variance")) + ylab(paste0("PC2: ",percentVar[2],"% variance")) + coord_fixed()
-
-#plot Heatmap
-heatmap.2(assay(rld),dendrogram="none",col=colorRampPalette(brewer.pal(name="RdBu",n=11))(200),Colv=FALSE,Rowv=FALSE,labRow=fdr20$`Gene symbol`,
-          labCol=rownames(df),main=i$name,ylab=paste("Significant Genes by log2FC",",",i$pValue,"FDR"),trace="none",key=TRUE,keysize=1.0,density.info="none",scale="row",margins=c(6,7))
+dev.off()
 
 
